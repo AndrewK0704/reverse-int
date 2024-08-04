52f758ca2348d92a1edc95d62979f02146578f49
andrewk0704 <andrew.kozhenov@mail.ru>
2024-08-04 18:46:25 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+
+    let arr1=n;
+    let arr2='';
+    let j;
+    
+    arr1=Math.abs(arr1);    
+    arr1=String(arr1);    
+    j=arr1.length-1;   
+
+    for (let i=0;i<arr1.length;i++){
+        arr2=arr2+arr1[j];
+        j--;
+    }
+
+    arr2=Number(arr2);
+    return(arr2);  
+    
 }
